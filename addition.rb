ee4d2dfd36d56759e30b13cb145ef8c4f42be7f9
Uday Prakash Dwivedi <uday.d@geitpl.com>
2021-05-29 11:04:47 +0530

    addition file with ruby code

---
+++ addition.rb
@@ -0,0 +1,22 @@
+puts "Enter The  first number  :"
+num1=gets.chomp.to_i
+
+puts  "Enter the Second Number :"
+
+num2=gets.chomp.to_i
+
+puts  "Enter The Third Number :"
+
+num3 = gets.chomp.to_i
+
+puts  "Enter The Fourth Number :"
+
+num4 = gets.chomp.to_i
+
+
+sum = num1+num2+num3+num4
+
+puts "your sum is : #{sum}"
+
+
+
